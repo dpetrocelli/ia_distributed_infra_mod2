0c64fab00a4e4fb3b5c2855e722ae636e152cc71
David Petrocelli <dmpetrocelli@gmail.com>
2025-11-21 00:01:05 +0000

    cleanupstage

--- docker/app/app.py
+++ docker/app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import psycopg2
 
 print ("Iniciando servidor web")
 
@@ -11,3 +12,27 @@ def home():
 @app.get("/status")
 def status():
     return {"status": "ok"}
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="db",
+        database="miapp",
+        user="usuario",
+        password="password123"
+    )
+    return conn
+
+# formas de tomar las variables
+# por ejemplo a través de variables de entorno
+# os.environ.get('DB_HOST')
+
+
+@app.get("/list_users")
+def list_users():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, name, email FROM users;")
+    users = cur.fetchall()
+    cur.close()
+    conn.close()
+    return {"users": users}
